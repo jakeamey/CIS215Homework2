dc3705513c5d2df67379eb76684846667760e963
jacob <jamey@genesee.edu>
2026-01-30 11:51:35 -0500

    Feat: Add time based greeting

--- homework2.php
+++ homework2.php
@@ -1,3 +1,15 @@
 <?php
 //set timezone
 date_default_timezone_set('America/New_York');
+
+//get current date and time
+$currenthour = (int)date('G'); 
+
+//determine greeting based on current hour
+if ($currenthour >= 5 && $currenthour < 12) {
+    $greeting = "Good Morning!";
+} elseif ($currenthour >= 12 && $currenthour < 18) {
+    $greeting = "Good Afternoon!";
+} else {
+    $greeting = "Good Evening!";
+}   
