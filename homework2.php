419db165767cc24d7690aeb5536dc529cb4a1b57
jacob <jamey@genesee.edu>
2026-02-02 13:56:44 -0500

    Feat:Add HTML Boilerplate and HTML greeting echo

--- homework2.php
+++ homework2.php
@@ -1,23 +1,39 @@
 <?php
-
-
-
-
+//Add HTML boilerplate
+echo "
+<!DOCTYPE html>
+<html lang = 'en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name= 'viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Homework2/title>
+</head>
+<body>
+";
+//Display php greeting as html heading    
+echo
+"<h1>$greeting</h1>";
+//Close HTML
+echo 
+"</body>
+</html>";
 ?>
+
 
 <?php
 //set timezone
 date_default_timezone_set('America/New_York');
-
 //get current date and time
-$currenthour = (int)date('G'); 
-
+$currenthour = (int)date('H'); 
 //determine greeting based on current hour
 if ($currenthour >= 5 && $currenthour < 12) {
     $greeting = "Good Morning!";
+    //If the time is less than noon, display Good Morning
 } elseif ($currenthour >= 12 && $currenthour < 18) {
     $greeting = "Good Afternoon!";
+    //If the time is greater than or equal to noon and less than 6, display Good Afternoon
 } else {
     $greeting = "Good Evening!";
+    //If the time is anything other than the 2 above, display Good Evening
 }   
 ?>
